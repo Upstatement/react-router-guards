3b9d9ba47a8e6d389f5c388d892bc170f06f94b5
Josh Pensky <joshuapensky@gmail.com>
2019-06-13 16:15:10 -0400

    removing integ test from Home page in basic demo

--- demos/basic/src/containers/Home.js
+++ demos/basic/src/containers/Home.js
@@ -1,34 +1,5 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React from 'react';
 
-const Home = ({ history }) => {
-  const [isHashUrl, setIsHashUrl] = useState(true);
-  const [value, setValue] = useState('');
-
-  const urlType = useMemo(() => (isHashUrl ? 'hash' : 'query'), [isHashUrl]);
-
-  const updateVal = ({ target }) => {
-    setValue(target.value);
-  };
-
-  const toggleUrlType = () => setIsHashUrl(value => !value);
-
-  useEffect(() => {
-    let url = `#${value}`;
-    if (!isHashUrl) {
-      url = `?value=${value}`;
-    }
-    history.replace(url);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isHashUrl, value]);
-
-  return (
-    <div>
-      <h1>Home</h1>
-      <h3>Update {urlType} URL</h3>
-      <input value={value} onChange={updateVal} />
-      <button onClick={toggleUrlType}>Toggle URL type</button>
-    </div>
-  );
-};
+const Home = () => <h1>Home</h1>;
 
 export default Home;
