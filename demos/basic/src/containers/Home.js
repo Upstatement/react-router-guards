c2209ad117c1723252b9c8d35fdaa8ee2d7061b9
Josh Pensky <joshuapensky@gmail.com>
2019-06-13 13:44:04 -0400

    adding integ testing of updating hash/query url

--- demos/basic/src/containers/Home.js
+++ demos/basic/src/containers/Home.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-const Home = () => <h1>Home</h1>;
+const Home = ({ history }) => {
+  const [isHashUrl, setIsHashUrl] = useState(true);
+  const [value, setValue] = useState('');
+
+  const urlType = useMemo(() => (isHashUrl ? 'hash' : 'query'), [isHashUrl]);
+
+  const updateVal = ({ target }) => {
+    setValue(target.value);
+  };
+
+  const toggleUrlType = () => setIsHashUrl(value => !value);
+
+  useEffect(() => {
+    let url = `#${value}`;
+    if (!isHashUrl) {
+      url = `?value=${value}`;
+    }
+    history.replace(url);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isHashUrl, value]);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <h3>Update {urlType} URL</h3>
+      <input value={value} onChange={updateVal} />
+      <button onClick={toggleUrlType}>Toggle URL type</button>
+    </div>
+  );
+};
 
 export default Home;
