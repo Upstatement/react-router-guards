02036d1389a71387c35b56992659beb12ed83ca5
Josh Pensky <joshuapensky@gmail.com>
2019-07-12 11:27:34 -0400

    moving guard validation logic into separate function file

--- package/src/Guard.tsx
+++ package/src/Guard.tsx
@@ -4,25 +4,8 @@ import { matchPath, Redirect, Route } from 'react-router-dom';
 import { ErrorPageContext, FromRouteContext, GuardContext, LoadingPageContext } from './contexts';
 import { usePrevious, useStateRef, useStateWhenMounted } from './hooks';
 import renderPage from './renderPage';
-import {
-  GuardFunction,
-  GuardProps,
-  GuardType,
-  GuardTypes,
-  Next,
-  NextAction,
-  NextPropsPayload,
-  NextRedirectPayload,
-} from './types';
-
-type PageProps = NextPropsPayload;
-type RouteError = string | Record<string, any> | null;
-type RouteRedirect = NextRedirectPayload | null;
-
-interface GuardsResolve {
-  props: PageProps;
-  redirect: RouteRedirect;
-}
+import validateGuardsForRoute from './validateGuardsForRoute';
+import { GuardProps, GuardToRoute, PageProps, RouteError, RouteRedirect } from './types';
 
 const Guard: React.FunctionComponent<GuardProps> = ({ children, component, meta, render }) => {
   const routeProps = useContext(RouterContext);
@@ -54,93 +37,22 @@ const Guard: React.FunctionComponent<GuardProps> = ({ children, component, meta,
   ]);
 
   /**
-   * Memoized callback to get the next callback function used in guards.
-   * Assigns the `props` and `redirect` functions to callback.
-   */
-  const getNextFn = useCallback((resolve: Function): Next => {
-    const getResolveFn = (type: GuardType) => (payload: NextPropsPayload | NextRedirectPayload) =>
-      resolve({ type, payload });
-
-    const next = () => resolve({ type: GuardTypes.CONTINUE });
-
-    return Object.assign(next, {
-      props: getResolveFn(GuardTypes.PROPS),
-      redirect: getResolveFn(GuardTypes.REDIRECT),
-    });
-  }, []);
-
-  /**
-   * Runs through a single guard, passing it the current route's props,
-   * the previous route's props, and the next callback function. If an
-   * error occurs, it will be thrown by the Promise.
-   *
-   * @param guard the guard function
-   * @returns a Promise returning the guard payload
-   */
-  const runGuard = (guard: GuardFunction): Promise<NextAction> =>
-    new Promise(async (resolve, reject) => {
-      try {
-        const to = {
-          ...routeProps,
-          meta: meta || {},
-        };
-        await guard(to, fromRouteProps, getNextFn(resolve));
-      } catch (error) {
-        reject(error);
-      }
-    });
-
-  /**
-   * Loops through all guards in context. If the guard adds new props
-   * to the page or causes a redirect, these are tracked in the state
-   * constants defined above.
-   */
-  const resolveAllGuards = async (): Promise<GuardsResolve> => {
-    let index = 0;
-    let props = {};
-    let redirect = null;
-    if (guards) {
-      while (!redirect && index < guards.length) {
-        const { type, payload } = await runGuard(guards[index]);
-        if (payload) {
-          if (type === GuardTypes.REDIRECT) {
-            redirect = payload;
-          } else if (type === GuardTypes.PROPS) {
-            props = Object.assign(props, payload);
-          }
-        }
-        index += 1;
-      }
-    }
-    return {
-      props,
-      redirect,
-    };
-  };
-
-  /**
    * Validates the route using the guards. If an error occurs, it
    * will toggle the route error state.
    */
   const validateRoute = async (): Promise<void> => {
     const currentRequests = validationsRequested.current;
 
-    let pageProps: PageProps = {};
-    let routeError: RouteError = null;
-    let routeRedirect: RouteRedirect = null;
-
-    try {
-      const { props, redirect } = await resolveAllGuards();
-      pageProps = props;
-      routeRedirect = redirect;
-    } catch (error) {
-      routeError = error.message || 'Not found.';
-    }
+    const to: GuardToRoute = {
+      ...routeProps,
+      meta: meta || {},
+    };
+    const { error, props, redirect } = await validateGuardsForRoute(guards, to, fromRouteProps);
 
     if (currentRequests === getValidationsRequested()) {
-      setPageProps(pageProps);
-      setRouteError(routeError);
-      setRouteRedirect(routeRedirect);
+      setPageProps(props);
+      setRouteError(error);
+      setRouteRedirect(redirect);
       setRouteValidated(true);
     }
   };
